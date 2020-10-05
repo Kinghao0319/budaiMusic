d4863ea4b3b6ce6778ca254576c6f3f05181c4ba
Martin Yin <qwqqw155@gmail.com>
2020-10-05 09:55:08 +0800

    初始化2020/10/5

---
+++ src/main/java/com/kinghao/budai/BudaiApplication.java
@@ -0,0 +1,13 @@
+package com.kinghao.budai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudaiApplication.class, args);
+    }
+
+}
